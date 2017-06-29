bf0bea2066a758a5219ca487f573e0124f2b4aac
Nikki Jedic <nikki.jedic@gmail.com>
2017-06-29 17:50:37 -0400

    Adding more Friends to the Array

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,6 +18,38 @@ var friendsArray = [
 			2,
 		]
 	},
+	{
+		name: "Regular-Sized Rudy",
+		photo: "http://i.imgur.com/o7Idhvh.png",
+		scores: [
+			3,
+			2,
+			1,
+			1,
+			5,
+			4,
+			1,
+			4,
+			4,
+			1,
+		]
+	},
+	{
+		name: "Hannibal Lecter",
+		photo: "https://s-media-cache-ak0.pinimg.com/736x/de/08/b9/de08b9a4514cd5e34c39947ae918372c--dr-hannibal-hannibal-lecter.jpg",
+		scores: [
+			5,
+			3,
+			5,
+			1,
+			3,
+			2,
+			3,
+			5,
+			1,
+			2,
+		]
+	},
 ];
 
 module.exports = friendsArray;
